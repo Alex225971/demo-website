b6f1afb99ae7e98b26c0e389f1b608b8be060ebf
Alex Haines <alex-haines1@hotmail.co.uk>
2022-05-07 14:25:56 +0100

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import { Welcome } from "./components/Welcome";
+import { Navigation } from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
       <div className="bg-dark text-light">
+        <Navigation />
         <Welcome name="Alex" />
       </div>
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,35 @@
 import React from "react";
 
-export function Navigation () {
-
+export function Navigation() {
+  return (
+    <nav class="d-flex navbar navbar-expand-lg navbar-light bg-light text-dark justify-content-between px-5">
+      <div class="navbar-nav navbar-logo">
+        <li class="navbar-brand">
+          <img src="img/logo" alt="Company Logo" />
+        </li>
+      </div>
+      <ul class="navbar navbar-nav navbar-expand-lg navbar-light bg-light">
+        <li class="nav-item">
+          <a class="nav-link active" href="/home">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/about">
+            About
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/gallery">
+            Gallery
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
