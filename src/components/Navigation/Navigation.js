85157438ee923333691cc1a08c5e1089fd2c8ce7
Alex Haines <alex-haines1@hotmail.co.uk>
2022-05-07 15:20:05 +0100

    Changed navbar styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,30 +2,31 @@ import React from "react";
 
 export function Navigation() {
   return (
-    <nav class="d-flex navbar navbar-expand-lg navbar-light bg-light text-dark justify-content-between px-5">
-      <div class="navbar-nav navbar-logo">
-        <li class="navbar-brand">
-          <img src="img/logo" alt="Company Logo" />
+    <nav className="d-flex navbar navbar-expand-lg navbar-light bg-light text-dark justify-content-between px-5">
+      <div className="navbar-nav navbar-logo">
+        <li className="navbar-brand">
+          {/* Don't know hwo to insert an image */}
+          <img src={"./logo.png"} alt="Company Logo" />
         </li>
       </div>
-      <ul class="navbar navbar-nav navbar-expand-lg navbar-light bg-light">
-        <li class="nav-item">
-          <a class="nav-link active" href="/home">
+      <ul className="navbar navbar-nav navbar-expand-lg navbar-light bg-light">
+        <li className="nav-item mx-3 current">
+          <a className="nav-link active current-link" href="/home">
             Home
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/about">
+        <li className="nav-item mx-3">
+          <a className="nav-link active" href="/about">
             About
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/gallery">
+        <li className="nav-item mx-3">
+          <a className="nav-link active" href="/gallery">
             Gallery
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/contact">
+        <li className="nav-item mx-3">
+          <a className="nav-link active" href="/contact">
             Contact
           </a>
         </li>
