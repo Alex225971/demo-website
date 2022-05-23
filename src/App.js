1f139646b06500a397423e8131cd2917519b9913
Alex Haines <alex-haines1@hotmail.co.uk>
2022-05-23 11:19:13 +0100

    Merged with contact

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import { Services } from "./components/Services/Services";
 import { Navigation } from "./components/Navigation/Navigation";
-<<<<<<< HEAD
 import About from "./components/About/About";
 import Gallery from "./components/Gallery/Gallery";
 import Footer from "./components/Footer/Footer";
 import Reviews from "./components/Reviews/Reviews";
-=======
-import { Contact } from "./components/Contact";
->>>>>>> feature/contact
+import Contact from "./components/";
 
 function App() {
   return (
@@ -17,14 +14,11 @@ function App() {
       <Navigation />
       <Header />
       <Services />
-<<<<<<< HEAD
       <About />
       <Gallery />
       <Reviews />
-      <Footer />
-=======
       <Contact />
->>>>>>> feature/contact
+      <Footer />
     </div>
   );
 }
